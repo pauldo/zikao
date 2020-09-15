7069215cb4b75013cd70f58bf165e1cf0ba9f28a
dupeng <dupeng@qudian.com>
2020-09-15 16:14:41 +0800

    fix bin

--- generate.sh
+++ generate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 folder=`pwd`
-if [ ! -d "$folder/c4" ]; then
+if [ ! -d "$folder/note" ]; then
     echo "请在项目根目录下运行"
     exit 1
 fi
 
-folders=`find c4 -type d|grep -v c4/base`
+folders=`find note -type d`
 for folder in $folders; do
     num=`ls -l $folder|grep ".puml"|wc -l`
     if [ $num -eq 0 ]; then
         continue
     fi
-    java -jar bin/plantuml.jar -o "output" -I "$folder/*.puml" -tsvg
+    #java -jar bin/plantuml.jar -o "output" -I "$folder/*.puml" -tsvg
     java -DPLANTUML_LIMIT_SIZE=24384 -jar bin/plantuml.jar -o "output" -I "$folder/*.puml" -tpng
 done
